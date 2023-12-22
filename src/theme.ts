ba725db167d791142a21d5d322e1133ae353c8a6
Ilshae <pruszynskaingrid@outlook.com>
2023-12-22 17:09:57 +0100

    theme and global styling - some typescript issues remaining

---
+++ src/theme.ts
@@ -0,0 +1,34 @@
+export const theme = {
+  color: {
+    white: "#ffffff",
+    blue: "#0298ba",
+    green: "#54a800",
+    pink: "#bf0040",
+    black: "#000",
+  },
+  fontFamily: {
+    text: "'Poppins', sans-serif",
+    title: "'Righteous', cursive",
+  },
+  screenSize: {
+    mobileS: "320px",
+    mobileM: "375px",
+    mobileL: "425px",
+    tablet: "768px",
+    laptop: "1024px",
+    laptopL: "1440px",
+    desktop: "1980px",
+    desktopL: "2560px",
+  },
+}
+
+export const device = {
+  mobileS: `(max-width: ${theme.screenSize.mobileS})`,
+  mobileM: `(max-width: ${theme.screenSize.mobileM})`,
+  mobileL: `(max-width: ${theme.screenSize.mobileL})`,
+  tablet: `(max-width: ${theme.screenSize.tablet})`,
+  laptop: `(max-width: ${theme.screenSize.laptop})`,
+  laptopL: `(max-width: ${theme.screenSize.laptopL})`,
+  desktop: `(max-width: ${theme.screenSize.desktop})`,
+  desktopL: `(min-width: ${theme.screenSize.desktop})`,
+}
